31f9526b80e09f9ef45d382d70ffd6a82f820d16
Alejandro Lopez <alejandro.malvaez@gmail.com>
2015-08-06 10:24:49 -0400

    Delete and update

--- Week2/Day2/Contacts/app/src/main/java/com/alexcompany/contacts/contacts/app/AppController.java
+++
@@ -1,9 +0,0 @@
-package com.alexcompany.contacts.contacts.app;
-
-import android.app.Application;
-
-/**
- * Created by Android1 on 8/5/2015.
- */
-public class AppController extends Application {
-}
